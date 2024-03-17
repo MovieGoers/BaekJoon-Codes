c2b7145cbc6fc30102b353cf7d7eb6533a74a2d7
CodingFeature <121998353+MovieGoers@users.noreply.github.com>
2024-03-17 15:02:17 +0900

    Create 30030_스위트콘_가격_구하기.cpp

---
+++ codes/30030_스위트콘_가격_구하기.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int corn;
+	cin >> corn;
+
+	cout << corn * 10 / 11;
+
+	return 0;
+}
