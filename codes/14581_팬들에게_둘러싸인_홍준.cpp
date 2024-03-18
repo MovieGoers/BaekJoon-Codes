81e479668117be01801948a256383ad9c6fc8bf0
CodingFeature <121998353+MovieGoers@users.noreply.github.com>
2024-03-18 17:39:32 +0900

    Create 14581_팬들에게_둘러싸인_홍준.cpp

---
+++ codes/14581_팬들에게_둘러싸인_홍준.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+
+	cout << ":fan::fan::fan:\n:fan::" << str << "::fan:\n:fan::fan::fan:";
+
+	return 0;
+}
