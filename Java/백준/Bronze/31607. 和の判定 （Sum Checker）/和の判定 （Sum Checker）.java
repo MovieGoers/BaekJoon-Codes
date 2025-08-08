018ece094ac4ef001523edb4702f9cbb93317590
Kim, Min Cheol <121998353+MovieGoers@users.noreply.github.com>
2025-08-09 00:21:38 +0900

    [Bronze IV] Title: 和の判定 (Sum Checker), Time: 68 ms, Memory: 11540 KB -BaekjoonHub

---
+++ Java/백준/Bronze/31607. 和の判定 （Sum Checker）/和の判定 （Sum Checker）.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[3];
+        for(int i=0;i<3;i++){
+            arr[i] = Integer.parseInt(br.readLine().trim());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.print((arr[0] + arr[1] == arr[2])? 1 : 0);
+    }
+}
